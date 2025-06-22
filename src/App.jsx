01e4b2e1277e1260aa6acbb2be09a1e71ad7e2c9
RobertLavrukevich <robertlavruk@gmail.com>
2025-06-22 20:03:16 +0300

    сделал страницу 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Routes, Route, Link } from 'react-router-dom';
 import HomeWeather from './pages/HomeWeather'
-import TenDaysWeather from './pages/TenDaysWeather'
+import SevenDaysWeather from './pages/SevenDaysWeather'
 import WorldWeather from './pages/WorldWeather'
 
 
@@ -11,13 +11,13 @@ function App() {
     <div className="headerBlock">
       <nav className='headerNav'>
         <Link className='firstBar' to="/">Главная</Link>{' '}
-        <Link className='secondBar' to="/10-days-weather">На 10 дней</Link>{' '}
+        <Link className='secondBar' to="/7-days-weather">На 7 дней</Link>{' '}
         <Link className='thirdBar' to="/world-weather">В мире</Link>
       </nav>
     </div>
       <Routes>
         <Route path="/" element={<HomeWeather />} />
-        <Route path="/10-days-weather" element={<TenDaysWeather />} />
+        <Route path="/7-days-weather" element={<SevenDaysWeather />} />
         <Route path="/world-weather" element={<WorldWeather />} />
       </Routes>
    
--- src/pages/TenDaysWeather.jsx
+++
@@ -1,8 +0,0 @@
-export default function TenDaysWeather() {
-  return (
-    <div>
-      <h1>Погода на 10 дней</h1>
-      <span>ВСё окей пацанв</span>
-    </div>
-  );
-}
